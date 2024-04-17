35f80d6d38a8c1712a341ab79e14b8a3ab40bfad
nano <marianosenduk@gmail.com>
2024-04-17 20:13:30 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/adproa3/microservicerequestproduct/MicroserviceRequestproductApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.adproa3.microservicerequestproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRequestproductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceRequestproductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/adproa3/microservicerequestproduct/MicroserviceRequestproductApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.adproa3.microservicerequestproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRequestproductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
